c392ea89e29bc4728fc3ab31339f58c0448d0385
Atos Alves <atosalvesqueiroz@gmail.com>
2025-04-01 21:18:30 -0300

    refactor: refatora método de login para usar URL de API do ambiente

--- services/login-service.ts
+++ services/login-service.ts
@@ -4,7 +4,7 @@ type LoginProps = {
 };
 
 export async function login(loginFormData: LoginProps) {
-    const response = await fetch("http://localhost:8080/login", {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",

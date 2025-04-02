b4be0c1088feb4910d369406192e018ffeb276c7
Atos Alves <atosalvesqueiroz@gmail.com>
2025-04-01 21:48:13 -0300

    refactor: reorganiza estrutura do componente de login

--- app/(publico)/login/page.tsx
+++ app/(publico)/login/page.tsx
@@ -1,38 +1,27 @@
-import { LoginForm } from "@/components/login-form";
+import { LoginForm } from "./components/login-form";
 import { Button } from "@/components/ui/button";
-import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardDescription,
-    CardContent,
-} from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 
 export default function LoginPage() {
     return (
-        <main className="flex flex-col w-full h-dvh items-center justify-center space-y-4">
-            <Link href="/" className="">
-                <Button variant="link" asChild>
-                    <div className="flex space-x-2">
-                        <ArrowLeft />
-                        Voltar para o catálogo
-                    </div>
-                </Button>
-            </Link>
+        <>
             <Card>
                 <CardHeader>
                     <CardTitle>Login</CardTitle>
-                    <CardDescription>
-                        Preencha seus dados previamente cadastrados para fazer
-                        login
-                    </CardDescription>
+                    <CardDescription>Preencha seus dados previamente cadastrados para fazer login</CardDescription>
                 </CardHeader>
                 <CardContent>
                     <LoginForm />
                 </CardContent>
             </Card>
-        </main>
+            <Button variant="link" asChild>
+                <Link href="/">
+                    <ArrowLeft />
+                    <span>Voltar para o catálogo</span>
+                </Link>
+            </Button>
+        </>
     );
 }

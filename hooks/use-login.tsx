43de27c1f27e0f1c50a652b866b66dd1b51ae765
Atos Alves <atosalvesqueiroz@gmail.com>
2025-04-01 21:46:12 -0300

    feat: implementa hook useLogin

--- hooks/use-login.tsx
+++ hooks/use-login.tsx
@@ -1,14 +1,41 @@
-import { login } from "@/services/login-service";
-import { useMutation } from "@tanstack/react-query";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { toast } from "sonner";
 import { useRouter } from "next/navigation";
-
+import { login } from "@/api/login.api";
+import { LoginFormData, loginSchema } from "@/schemas/login.schema";
 
 export function useLogin() {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const form = useForm<LoginFormData>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
     const router = useRouter();
 
-    return useMutation({
-        mutationFn: login,
-        onSuccess: () => router.push("/dashboard"),
-    });
-}
+    function onSubmit(loginFormData: LoginFormData) {
+        setIsLoading(true);
+        toast.promise(
+            async () => {
+                await login(loginFormData);
+            },
+            {
+                success: () => {
+                    router.push("/dashboard");
+                    return "Logado com sucesso";
+                },
+                error: ({ message }) => message,
+                loading: "Carregando...",
+                finally: () => setIsLoading(false),
+            }
+        );
+    }
 
+    return { form, onSubmit, isLoading };
+}

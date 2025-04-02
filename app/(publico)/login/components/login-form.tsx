1f5e0c75ba5d137e98272f819753f75449c71383
Atos Alves <atosalvesqueiroz@gmail.com>
2025-04-01 21:45:40 -0300

    refactor: atualiza login-form pós remoção do tanstack

---
+++ app/(publico)/login/components/login-form.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useLogin } from "@/hooks/use-login";
+
+export function LoginForm() {
+    const { form, onSubmit, isLoading } = useLogin();
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input type="email" placeholder="Email" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Senha</FormLabel>
+                            <FormControl>
+                                <Input type="password" placeholder="Senha" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" disabled={isLoading}>
+                    Entrar
+                </Button>
+            </form>
+        </Form>
+    );
+}

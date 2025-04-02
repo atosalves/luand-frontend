eb2e93028b54b01fc89a35391a5ca211200a5231
Atos Alves <atosalvesqueiroz@gmail.com>
2025-04-01 21:36:54 -0300

    refactor: refatora login.api.ts para utilizar o tipo correto

---
+++ api/login.api.ts
@@ -0,0 +1,19 @@
+import { LoginFormData } from "@/schemas/login.schema";
+
+export async function login(loginFormData: LoginFormData) {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginFormData),
+        credentials: "include",
+    }).catch(() => {
+        throw new Error("Erro no servidor");
+    });
+
+    if (!response.ok) {
+        const data = await response.json();
+        throw new Error(data.message);
+    }
+}
